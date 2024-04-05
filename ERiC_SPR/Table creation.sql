62eb043264dafb5ac9531dca5e15122ca2351d87
Anuradha Samaranayake <Anuradha_10436@dialog.lk>
2024-04-05 12:41:56 +0530

    i committtt

---
+++ ERiC_SPR/Table creation.sql
@@ -0,0 +1,31 @@
+CREATE TABLE SLOC (
+    `Descr. of Storage Loc.` VARCHAR(255),
+    Plant INT,
+    `Storage location` INT,
+    Material INT PRIMARY KEY,
+    `Material Description` VARCHAR(255),
+    Unrestricted INT,
+    `Base Unit of Measure` INT,
+    `Value Unrestricted` INT,
+    `Quality Inspection` INT,
+    `Value in QualInsp.` INT,
+    `Friendly Name` VARCHAR(255),
+    Region VARCHAR(255),
+    OPER INT
+);
+
+CREATE TABLE SLOC (
+    `Descr. of Storage Loc.` VARCHAR(255),
+    Plant INT,
+    `Storage location` INT,
+    Material INT PRIMARY KEY,
+    `Material Description` VARCHAR(255),
+    Unrestricted INT,
+    `Base Unit of Measure` INT,
+    `Value Unrestricted` INT,
+    `Quality Inspection` INT,
+    `Value in QualInsp.` INT,
+    `Friendly Name` VARCHAR(255),
+    Region VARCHAR(255),
+    OPER INT
+);
